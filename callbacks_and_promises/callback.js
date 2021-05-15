91fd3be09db05ded1b3df68d41f7bdd0d027a15d
abhishekchandra2522k <1ac23456789@gmail.com>
2021-05-15 22:02:15 +0530

    Add callbacks_and_promises

---
+++ callbacks_and_promises/callback.js
@@ -0,0 +1,10 @@
+const posts = [
+    {title : 'Post One', body : 'This is post one'},
+    {title : 'Post Two', body : 'This is post two'},
+];
+
+function getPosts() {
+    setTimeout(function(){
+        let output = '';
+    }, 1000);
+}
