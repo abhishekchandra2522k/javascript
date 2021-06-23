6775fa9727fca4430a6e6643b857f710ecb6d5fa
abhishekchandra2522k <1ac23456789@gmail.com>
2021-06-23 22:01:27 +0530

    Add mysql nodejs integration files

--- nodejs/mysql_create_table.js
+++ nodejs/mysql_create_table.js
@@ -4,7 +4,7 @@ var con = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "password",
-    "database": 'mydb'
+    database: 'mydb'
 });
 
 con.connect(function(err){
