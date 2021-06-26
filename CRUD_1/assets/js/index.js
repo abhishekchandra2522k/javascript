08633195c2185d977849e3ac56080960bffd8274
abhishekchandra2522k <1ac23456789@gmail.com>
2021-06-26 11:11:46 +0530

    Update CRUD_1

---
+++ CRUD_1/assets/js/index.js
@@ -0,0 +1,45 @@
+ $("#add_user").submit(function(event){
+     alert("Data inserted, success!")
+ })
+
+ $('#update_user').submit(function(event){
+     event.preventDefault();
+
+     var unindexed_array = $(this).serializeArray();
+     var data = {}
+
+     $.map(unindexed_array,function(n,i){
+        data[n['name']] = n['value']
+     })
+     console.log(data);
+
+     var request = {
+         "url" : `https://localhost:3000/api/users/${data.id}`,
+         "method" : "PUT",
+         "data": data  
+     }
+
+     $.ajax(request).done(function(response){
+         alert("Data Updated Successfully");
+     })
+ })
+
+ if(window.location.pathname == "/"){
+     $ondelete = $(".table tbody td a.delete");
+     $ondelete.click(function(){
+         var id = $(this).attr("data-id");
+
+         var request = {
+            "url" : `https://localhost:3000/api/users/${data.id}`,
+            "method" : "DELETE",
+            "data": data  
+        }
+
+        if(confirm("Do you really want to delete this record?")){
+            $.ajax(request).done(function(response){
+                alert("Data Deleted Successfully");
+                location.reload;
+            })
+        }
+     })
+ }
--- CRUD_1/server.js
+++ CRUD_1/server.js
@@ -6,6 +6,8 @@ const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 const path = require('path');
 
+const connectDB = require('./server/database/connection');
+
 const app = express();
 
 dotenv.config({path: 'config.env'})
@@ -13,6 +15,9 @@ const PORT = process.env.PORT || 8080
 
 // log requests
 app.use(morgan('tiny'));
+
+// mongodb connection
+connectDB();
 
 // parse request to body-parser
 app.use(bodyParser.urlencoded({extended: true}));
@@ -26,12 +31,13 @@ app.use('/css',express.static(path.resolve(__dirname,"assets/css")));
 app.use('/img',express.static(path.resolve(__dirname,"assets/img")));
 app.use('/js',express.static(path.resolve(__dirname,"assets/js")));
 
-app.get('/', (req, res)=>{
-    res.render("index");
-})
 
-app.get('/', (req, res)=>{
-    res.send("Crud Application");
-})
+
+// app.get('/', (req, res)=>{
+//     res.send("Crud Application");
+// })
+
+// load routers 
+app.use('/',require('./server/routes/router'))
 
 app.listen(PORT,()=>{console.log(`Server is running on https://localhost:${PORT}`)});
