62c089e722be817568171f9177e2e94f75f60a7d
abhishekchandra2522k <1ac23456789@gmail.com>
2022-11-19 18:15:48 +0530

    Refactor the repo structure, added a project folder and crah course folder

---
+++ CodevolutionCrashCourse/script.js
@@ -0,0 +1,32 @@
+// log a statement using console.log()
+console.log('hello from script.js')
+
+// variables
+let age = 25
+console.log(age)
+
+const salary = 8000 // must be initialized and cannot be changed, like final keyword in java
+console.log(salary)
+
+let sum = 0
+sum = 5
+console.log(sum)
+
+// data types
+
+// strings
+const firstName = 'Abhishek'
+const lastName = 'Chandra'
+const language = `JavaScript` // a string can have single quotes or double quotes or backticks
+
+// float / number
+const total = 0
+const PI = 3.14
+
+// booleans
+const isPrimaryNumber = true
+const isNewUser = false
+
+// undefined: if the variable is declared but a value is not assigned to it
+let result
+console.log(result)
